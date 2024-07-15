7a42482cdec4aa366469fa9930f209fba503e9b7
ChargedBathtub <abderrahman.loukil@ensi-uma.tn>
2024-07-15 12:28:39 +0100

    Initial author routes

--- server.js
+++ server.js
@@ -7,15 +7,19 @@ const express = require('express')
 const app=express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexrouter = require ('./routes/index')
+const indexRouter = require ('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require ('body-parser')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10 mb', extended:false}))
 
-app.use('/',indexrouter)
+app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
