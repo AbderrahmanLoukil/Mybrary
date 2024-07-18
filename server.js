43199d1b34c5e82def58e10c3de14d69e4c1f178
ChargedBathtub <abderrahman.loukil@ensi-uma.tn>
2024-07-18 10:18:46 +0100

    Finished book routes

--- server.js
+++ server.js
@@ -19,11 +19,9 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit:'10 mb', extended:false}))
-app.use('/books',bookRouter)
 app.use(methodOverride('_method'))
 
-app.use('/',indexRouter)
-app.use('/authors',authorRouter)
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
@@ -31,6 +29,8 @@ const db = mongoose.connection
 db.on('error',error=>{console.error(error)})
 db.once('open',()=>{console.log('Connection to mongoose Successful')})
 
-
+app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3000)
